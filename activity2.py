e9e0f037c287f3492d13d211648141ca29e3d94e
kamalisthegoat <manjusundaram1983@gmail.com>
2026-05-10 16:12:07 +0530

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,21 @@
+try:
+    num1, num2 = eval(input("enter two numbers, separated by a comma :"))
+    result = num1 / num2
+    print("Reult is", result)
+
+#using multiple except block for different type of error    
+
+except ZeroDivisionError:
+    print("division by zero is a error!!!!!!!")
+
+except SyntaxError:
+    print("comma is missing. enter numbers separated by comma like this 4,20")
+
+except:
+    print("wrong input")
+
+else:
+    print("no exceptions")
+
+finally:
+    print("this will execute no matter what")                    
